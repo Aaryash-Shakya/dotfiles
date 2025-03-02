ffde39e2a910e9ee8d61df97ad7fab469a46bb7e
Aaryash-Shakya <aaryashshakya.dev@gmail.com>
2025-03-02 17:05:49 +0545

    add: update scripts

--- scripts/cmd/tangible.sh
+++ scripts/cmd/tangible.sh
@@ -10,18 +10,18 @@ tmux new-session -d -s dev
 # Create first window named 'back' and split into two panes
 # Both panes will start in /home/aaryash-u/Documents/arbyte/tangible-backend
 tmux rename-window -t dev:1 back
-tmux send-keys -t dev:1 "cd /home/aaryash-u/Documents/arbyte/tangible-backend" C-m
+tmux send-keys -t dev:1 "cd /home/aaryash-f/Documents/arbyte/tangible-backend" C-m
 tmux send-keys -t dev:1 "yarn dev" C-m
 tmux split-window -h -t dev:1
-tmux send-keys -t dev:1.1 "cd /home/aaryash-u/Documents/arbyte/tangible-backend" C-m
+tmux send-keys -t dev:1.1 "cd /home/aaryash-f/Documents/arbyte/tangible-backend" C-m
 
 # Create second window named 'front' and split into two panes
 # Both panes will start in /home/aaryash-u/Documents/arbyte/tangible-frontend
 tmux new-window -t dev:2 -n front
-tmux send-keys -t dev:2 "cd /home/aaryash-u/Documents/arbyte/tangible-frontend" C-m
+tmux send-keys -t dev:2 "cd /home/aaryash-f/Documents/arbyte/tangible-frontend" C-m
 tmux send-keys -t dev:2 "yarn dev" C-m
 tmux split-window -h -t dev:2
-tmux send-keys -t dev:2.1 "cd /home/aaryash-u/Documents/arbyte/tangible-frontend" C-m
+tmux send-keys -t dev:2.1 "cd /home/aaryash-f/Documents/arbyte/tangible-frontend" C-m
 
 # Attach to the session
 #tmux attach-session -t dev
--- scripts/cmd/tech-disk-mount.sh
+++ scripts/cmd/tech-disk-mount.sh
@@ -1,3 +1,3 @@
 sudo umount /dev/sda1
 sudo ntfsfix /dev/sda1
-sudo mount -t ntfs-3g /dev/sda1 /media/aaryash-u/Tech\ Disk
+sudo mount -t ntfs-3g /dev/sda1 /run/media/aaryash-f/Tech\ Disk
